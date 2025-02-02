e50cec9e0f80ea703ee94c132f5942769026fded
yboateng94 <ybdecipher@outlook.com>
2025-02-02 15:48:31 +0000

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -2,7 +2,7 @@
 USE {{alx_book_store}};
 
 -- Insert multiple rows into the Customers table
-INSERT INTO ["INSERT INTO customer", ["125 Happiness  Ave."]]
+INSERT INTO "INSERT INTO customer", ["125 Happiness  Ave."]
 VALUES 
     (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
     (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
