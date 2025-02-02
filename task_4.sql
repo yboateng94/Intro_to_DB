294ffab7c3ac930f91f2270327c2c99057edf742
yboateng94 <ybdecipher@outlook.com>
2025-02-02 15:32:00 +0000

    Create task_4.sql

---
+++ task_4.sql
@@ -0,0 +1,15 @@
+-- Use the database passed as an argument
+["USE alx_book_store";]
+
+-- Query to get the full description (columns and their details) of the 'books' table
+SELECT 
+    COLUMN_NAME, 
+    COLUMN_TYPE, 
+    IS_NULLABLE, 
+    COLUMN_DEFAULT, 
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = '{{alx_book_store}}'
+    AND TABLE_NAME = 'Books';
